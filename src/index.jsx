7f3de4a6762d2c448d91ce14c5350f3990668220
Richard Erdös Chalk <richard.erdos-chalk@sweetsystems.se>
2022-06-22 13:12:35 +0200

    Starting template

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+import App from "./App";
+
+// Version 18 syntax
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App/>
+)
