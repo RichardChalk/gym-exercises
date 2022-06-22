97c5774a59c4e63ecde909ed4915186ee0c61e79
Richard Erdös Chalk <richard.erdos-chalk@sweetsystems.se>
2022-06-22 13:18:28 +0200

    Installed dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
 // Version 18 syntax
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <App/>
-)
+root.render(<App/>)
